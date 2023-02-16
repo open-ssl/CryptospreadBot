be9f1ba79f5e1ed2f8fced16eee8c1e742d80cf2
Stanislav Lukyanov <>
2023-02-16 22:23:07 +0400

    revoke token for bot

--- bot_config.py
+++ bot_config.py
@@ -3,7 +3,7 @@
 import telebot
 
 
-API_TOKEN = '5686307192:AAGWxHH_auacIqrN6gXK2F3cBKJnpFqdXyg'
+API_TOKEN = ''
 GENERATED_CODE_LENGTH = 16
 # CREATE_NEW_CODE_END_POINT = 'http://cryptospread.net/create_code_for_user'
 CREATE_NEW_CODE_END_POINT = 'http://127.0.0.1:5002/create_code_for_user'
